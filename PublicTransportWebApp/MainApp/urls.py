da083616824cc979983d9dc94cbd99c42a95e629
MadeYoga <vngla21@gmail.com>
2018-10-21 17:54:50 +0700

    updates home design

--- PublicTransportWebApp/MainApp/urls.py
+++ PublicTransportWebApp/MainApp/urls.py
@@ -7,6 +7,9 @@ from . import views
 urlpatterns = [
     url(r"^$", views.home, name="index"),
     url(r"^contact/", views.contact, name="contact"),
-    url(r"^home/(?P<pk>\d+)$", views.info, name='info'),
+    url(r"^schedule_list/(?P<pk>\d+)$", views.schedule_list, name='list'),
+    url(r"^schedule_detail/(?P<pk>\d+)$", views.schedule_detail, name='detail'),
     url(r"^login/", views.login, name='login'),
+    url(r"^new_home/", views.new_home, name="new_home"),
+    url(r"^map/", views.map, name="new_home"),
 ]
--- PublicTransportWebApp/MainApp/views.py
+++ PublicTransportWebApp/MainApp/views.py
@@ -6,33 +6,44 @@ def index(request):
     return render(request, "MainApp/header.html")
 
 def home(request):
-    all_categories = Category.objects.all()
-    categories_dictionary = {'all_categories' : all_categories}
+    all_categories          = Category.objects.all()
+    categories_dictionary   = {'all_categories' : all_categories}
     return render(request, "MainApp/home.html",categories_dictionary)
 
-def info(request, pk):
-    query = "SELECT * FROM MAINAPP_SCHEDULE S JOIN MAINAPP_TRANSPORTATION T ON S.TRANSPORTATION_ID_ID = T.id JOIN MAINAPP_CATEGORY C ON T.CATEGORY_ID_ID = C.id WHERE C.id = " + pk
-    all_schedule = Schedule.objects.raw(query)
+def map(request):
+    return render(request, "MainApp/map.html")
+
+def new_home(request):
+    return render(request, "MainApp/new_home.html")
+
+def schedule_list(request, pk):
+    query               = "SELECT * FROM MAINAPP_SCHEDULE S JOIN MAINAPP_TRANSPORTATION T ON S.TRANSPORTATION_ID_ID = T.id JOIN MAINAPP_CATEGORY C ON T.CATEGORY_ID_ID = C.id WHERE C.id = " + pk
+    all_schedule        = Schedule.objects.raw(query)
     schedule_dictionary = {'all_schedule' : all_schedule}
     # category = Category.objects.all().get(pk=pk)
     # RETURNS 0 OR MORE
     # transportations = Transportation.objects.all().filter(category_id=category)
     # transportations_dictionary = {'all_transportations' : transportations}
-    return render(request, "MainApp/info.html", schedule_dictionary)
+    return render(request, "MainApp/schedule_list.html", schedule_dictionary)
+
+def schedule_detail(request, pk):
+    query                       = "SELECT * FROM MAINAPP_SCHEDULE S WHERE S.id = " + pk
+    schedule                    = Schedule.objects.raw(query)
+    schedule_detail_dictionary  = {"schedule" : schedule}
+    return render(request, "MainApp/schedule_detail.html", schedule_detail_dictionary)
 
 def login(request):
     if request.method == 'GET':
         return render(request, "MainApp/login.html", {'response' : ""})
     else:
-        input_mail = request.POST.get("inputEmail", "")
-        input_pass = request.POST.get("inputPassword", "")
-        found_account = Account.objects.filter(email=input_mail, password=input_pass)
+        input_mail      = request.POST.get("inputEmail", "")
+        input_pass      = request.POST.get("inputPassword", "")
+        found_account   = Account.objects.filter(email=input_mail, password=input_pass)
         if not found_account:
             return render(request, "MainApp/login.html", {'response' : False})
         else:
             return home(request)
         
-
 def contact(request):
-    content = {'content' : ['if you would like to contact me, please mail me at', 'mail@mail.com']}
+    content = {'content' : ['if you would like to contact me, please mail me at', 'm26416083@john.petra.ac.id']}
     return render(request, "MainApp/basic.html", content)
